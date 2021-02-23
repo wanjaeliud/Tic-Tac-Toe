27755e80bfbac8c9faedaf89f16da55cbaad4a72
Eliud Wanja <elkwanja@gmail.com>
2021-02-23 23:46:39 +0300

    spiral rotate an matrix of size nxn

--- Problems/Spiral/src/Main.java
+++ Problems/Spiral/src/Main.java
@@ -1,9 +1,45 @@
 import java.util.Scanner;
-class Main {
+public class Main {
+
     public static void main(String[] args) {
-        // put your code here
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[][] num = new int[n][n];
+        int dimension = scanner.nextInt();
+        print2dArray(getSpiralArray(dimension));
+    }
+
+    public static int[][] getSpiralArray(int dimension) {
+        int[][] spiralArray = new int[dimension][dimension];
+        int sideLen = dimension;
+        int currNum = 1;
+
+        for (int i = 0; i < dimension; i++) {
+            // do top side
+            for (int j = 0; j < sideLen; j++) {
+                spiralArray[i][i + j] = currNum++;
+            }
+            // do right side
+            for (int j = 1; j < sideLen; j++) {
+                spiralArray[i + j][dimension - 1 - i] = currNum++;
+            }
+            // do bottom side
+            for (int j = sideLen - 2; j > -1; j--) {
+                spiralArray[dimension - 1 - i][i + j] = currNum++;
+            }
+            // do left side
+            for (int j = sideLen - 2; j > 0; j--) {
+                spiralArray[i + j][i] = currNum++;
+            }
+            sideLen -= 2;
+        }
+        return spiralArray;
+    }
+    public static void print2dArray(int[][] array) {
+        for (int[] row : array) {
+            for (int elem : row) {
+//                System.out.print(elem + " ");
+                System.out.printf("%4d", elem);
+            }
+            System.out.println();
+        }
     }
 }
